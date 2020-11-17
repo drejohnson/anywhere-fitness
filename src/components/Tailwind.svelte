35417f40991fb4bc68ad56a5261b294ad88cb9d7
Dre Johnson <dre@phreshr.com>
2020-11-18 05:17:56 +0800

    add tailwindcss imports

---
+++ src/components/Tailwind.svelte
@@ -0,0 +1,29 @@
+<style global lang="postcss">
+  @import "tailwindcss/base";
+  @import "tailwindcss/components";
+  @import "tailwindcss/utilities";
+
+  @layer components {
+    .page-title {
+      @apply mt-2 max-w-3xl text-2xl font-semibold text-gray-900;
+    }
+
+    .btn {
+      @apply font-bold py-2 px-4 rounded;
+    }
+
+    .btn-blue {
+      @apply bg-blue-500 text-white;
+    }
+    .btn-blue:hover {
+      @apply bg-blue-700;
+    }
+
+    .btn-gray {
+      @apply bg-gray-400 text-gray-800;
+    }
+    .btn-gray:hover {
+      @apply bg-gray-500;
+    }
+  }
+</style>
