1149329f58b8a4f6ea37b9e2f25f240bff957af1
Dre Johnson <dre@phreshr.com>
2020-11-22 22:01:52 +0800

    add profile route

---
+++ src/components/Profile.svelte
@@ -0,0 +1,14 @@
+<script lang="typescript">
+  import type { User } from "../stores/auth";
+  export let user: User | undefined;
+</script>
+
+<div>
+  {#if user}
+    <h3>Hi {user.firstName} {user.lastName}</h3>
+
+    <p>Your id is {user._id}</p>
+    <p>Your email is {user.email}</p>
+    <p>Your role is {user.role}</p>
+  {/if}
+</div>
---
+++ src/routes/Profile.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+  import { auth, Status } from "../stores/auth";
+  import Profile from "../components/Profile.svelte";
+  import Logout from "../components/Logout.svelte";
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+{#if $auth.status === Status.AUTHENTICATED}
+  <Profile user={$auth.user} />
+  <Logout />
+{:else}
+  <h3>Try logging in</h3>
+{/if}
