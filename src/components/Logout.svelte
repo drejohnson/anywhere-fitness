c73736b0871dd4c6000bfbe2a666b17166049058
Dre Johnson <dre@phreshr.com>
2020-11-22 22:07:06 +0800

    added logut button component

---
+++ src/components/Logout.svelte
@@ -0,0 +1,27 @@
+<script lang="typescript">
+  import { navigate } from "svelte-navigator";
+  import { request, gql } from "../graphql/request";
+  import { auth, Status } from "../stores/auth";
+
+  const query = gql`
+    mutation logoutUser {
+      logoutUser
+    }
+  `;
+
+  let handleLogout = async () => {
+    await request(query);
+    auth.set({ status: Status.NOT_AUTHENTICATED });
+    // navigate('/', {replace: true})
+  };
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<button
+  class="text-2xl text-white hover:text-black font-display border-2 border-solid border-white py-2 px-16 focus:outline-none hover:bg-white rounded-full"
+  on:click|preventDefault={handleLogout}>
+  Log out
+</button>
