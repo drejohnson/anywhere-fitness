a3d6a5c53551d03d9080eb05f6bd9bdaad65772e
Dre Johnson <dre@phreshr.com>
2020-11-22 22:05:10 +0800

    refactored, added auth route

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="typescript">
-	import { Router, Link, Route, link } from "svelte-navigator";
-	import NavLink from "./components/NavLink.svelte";
+	import { Router, Route } from "svelte-navigator";
+	import Tailwind from "./components/Tailwind.svelte";
+	import Header from "./components/Header.svelte";
 	import Home from "./routes/Home.svelte";
 	import About from "./routes/About.svelte";
-	import Tailwind from "./components/Tailwind.svelte";
+	import Browse from "./routes/Browse.svelte";
+	import Login from "./routes/Login.svelte";
+	import Profile from "./routes/Profile.svelte";
+	import Signin from "./routes/Signin.svelte";
 </script>
 
 <style lang="postcss">
@@ -18,42 +22,33 @@
 
 <Tailwind />
 <Router>
-	<header
-		class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-		<a
-			href="/"
-			use:link
-			class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-				viewBox="0 0 24 24">
-				<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-			</svg>
-			<span class="ml-3 text-xl">Svelte App</span>
-		</a>
+	<div class="relative">
+		<Header />
+		<main>
+			<Route path="/">
+				<Home />
+			</Route>
+			<Route path="/browse">
+				<Browse />
+			</Route>
+			<Route path="/about">
+				<About />
+			</Route>
+			<Route path="/auth/client/*">
+				<Route path="/login">
+					<Signin />
+				</Route>
+			</Route>
 
-		<nav
-			class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-			<NavLink to="/" styles="ml-5 hover:text-gray-900">Home</NavLink>
-			<NavLink to="about" styles="ml-5 hover:text-gray-900">About</NavLink>
-		</nav>
-	</header>
-	<main>
-		<Route path="/">
-			<Home />
-		</Route>
-		<Route path="/about">
-			<About />
-		</Route>
-		<Route>
-			<h3>Oops...</h3>
-			<p>No Route could be matched.</p>
-		</Route>
-	</main>
+			<Route path="/user/*">
+				<Route path="/profile">
+					<Profile />
+				</Route>
+			</Route>
+			<Route>
+				<h3>Oops...</h3>
+				<p>No Route could be matched.</p>
+			</Route>
+		</main>
+	</div>
 </Router>
