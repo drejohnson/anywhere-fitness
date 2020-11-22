afe305786b319832081abf330ced94b3da202ab7
Dre Johnson <dre@phreshr.com>
2020-11-22 22:02:20 +0800

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="typescript">
+  import { link } from "svelte-navigator";
+  import NavLink from "./NavLink.svelte";
+</script>
+
+<style lang="postcss">
+  .logo {
+    width: 200px;
+  }
+  .navlink {
+    @apply font-display text-2xl ml-5 text-white;
+  }
+</style>
+
+<header
+  class="absolute w-full flex flex-wrap p-5 flex-col md:flex-row items-center z-50">
+  <a href="/" use:link class="logo">
+    <img src="/logo.svg" alt="Anywhere Fitness Logo" />
+  </a>
+
+  <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+    <NavLink to="browse"><span class="navlink">Browse</span></NavLink>
+    <NavLink to="about"><span class="navlink">About</span></NavLink>
+    <NavLink to="auth/client/login">
+      <span
+        class="font-display text-2xl ml-5 text-white hover:text-black border-2 border-solid border-white hover:bg-white rounded-full py-1 px-6">Sign
+        In</span>
+    </NavLink>
+  </nav>
+</header>
