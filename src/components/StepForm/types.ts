40df0f41d37f7c41fdef1af5354132a9021b1c36
Dre Johnson <dre@phreshr.com>
2020-11-24 12:35:44 +0800

    delete step form

--- src/components/StepForm/types.ts
+++
@@ -1,4 +0,0 @@
-export type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue };
-
-export type JsonString = { [key: string]: string };
-export type JsonBool = { [key: string]: boolean };
