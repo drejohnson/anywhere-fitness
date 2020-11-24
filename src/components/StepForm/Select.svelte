d959c6fb2cf8403012a1cd0b75999df69a2fac21
Dre Johnson <dre@phreshr.com>
2020-11-24 12:33:24 +0800

    initial smulti step form setup

---
+++ src/components/StepForm/Select.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import type { Writable } from "svelte/store";
+
+  import type { JsonString } from "../types";
+
+  export let store: Writable<JsonString>;
+  export let name: string;
+
+  const dispatch = createEventDispatcher();
+
+  let value: string;
+
+  store.subscribe((v) => (value = v[name]));
+
+  const onChange = (e: FocusEvent) => {
+    store.update((v) => {
+      v[name] = value;
+
+      return v;
+    });
+
+    dispatch("change", e);
+  };
+</script>
+
+<select bind:value on:blur={onChange} {name} {...$$restProps}>
+  <slot />
+</select>
