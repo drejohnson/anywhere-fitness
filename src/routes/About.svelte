e4d10815a228e008b5040e40b7e78d47b6024af4
Dre Johnson <dre@phreshr.com>
2020-11-28 03:31:00 +0800

    added initial styles

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,7 +1,24 @@
-<script>
+<script lang="typescript">
+  import { count } from "../stores/count";
 </script>
 
 <style lang="postcss">
+  /* your styles go here */
+  .bg-image {
+    @apply h-screen bg-cover bg-bottom;
+  }
 </style>
 
-<h1 class="page-title">About</h1>
+<div class="lg:flex">
+  <div class="w-full">
+    <section class="bg-image bg-black flex justify-center items-center">
+      <div class="w-full pt-24 px-64 z-10 text-white">
+        <h1>The count is {$count}</h1>
+
+        <button on:click={count.increment}>+</button>
+        <button on:click={count.decrement}>-</button>
+        <button on:click={count.reset}>reset</button>
+      </div>
+    </section>
+  </div>
+</div>
