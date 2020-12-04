1ddcd4475a9c7f323aaa024494812f0a98d724f4
Dre Johnson <dre@phreshr.com>
2020-12-05 00:45:57 +0800

    added more aliases

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,4 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
+const fs = require('fs-extra');
+const path = require('path');
+
 module.exports = {
   mount: {
     public: '/',
@@ -8,22 +11,43 @@ module.exports = {
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
-    '@snowpack/plugin-optimize',
+    ["@snowpack/plugin-optimize", {
+      preloadModules: true,
+      preloadCSS: true,
+      target: 'es2018'
+    }],
 
     [
       '@snowpack/plugin-run-script',
       {cmd: 'svelte-check --output human --ignore "lib,functions,api,fql"', watch: '$1 --watch', output: 'stream'},
     ],
 
-    [
-      "snowpack-plugin-rollup-bundle",
-      {
-        emitHtmlFiles: false,
-        preserveSourceFiles: true,
-        // equivalent to inputOptions.input from Rollup
-        entrypoints: "build/_dist_/index.js",
-      }
-    ]
+    // [
+    //   "snowpack-plugin-rollup-bundle",
+    //   {
+    //     emitHtmlFiles: true,
+    //     preserveSourceFiles: true,
+    //     // equivalent to inputOptions.input from Rollup
+    //     entrypoints: "build/_dist_/index.js",
+    //     extendConfig: (config) => {
+    //       config.inputOptions.plugins.push(
+    //         {
+    //           name: 'copy-assets',
+    //           generateBundle() {
+    //             fs.copySync(
+    //               path.resolve('./public'),
+    //               path.resolve('./build'), {
+    //                 dereference: true,
+    //                 filter: file => file !== path.resolve('public/index.html')
+    //               }
+    //             );
+    //           }
+    //         }
+    //       )
+    //       return config
+    //     }
+    //   }
+    // ]
   ],
   install: [
     /* ... */
@@ -46,10 +70,13 @@ module.exports = {
   alias: {
     "@app": "./src",
     "@components": "./src/components",
+    "@routes": "./src/routes",
     "@stores": "./src/stores",
+    "@actions": "./src/actions",
     "@utils": "./src/utils",
     "@api": "./api",
     "@lib": "./lib",
     "@types": "./types"
   },
 };
+
