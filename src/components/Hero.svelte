3e7caf45ccf1b2f279b4c310aff37577cedacb6f
Dre Johnson <dre@phreshr.com>
2020-12-06 02:35:10 +0800

    lazy load image

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+import lazyload from '@actions/lazyload'
+
+
   export let image: string;
+  export let image_placeholder: string = "";
   export let styles: string = "";
   export let gradient = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))"
+
 </script>
 
 <style lang="postcss">
 </style>
 
-<section 
+<section
+  use:lazyload
   class="grid md:auto-cols-fr gap-x-4 min-h-screen bg-cover bg-center items-center justify-items-center relative {styles}"
   style="background-image: {gradient}, url({image})"
   {...$$restProps}>
