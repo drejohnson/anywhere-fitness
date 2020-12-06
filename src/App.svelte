658151c8136c08a31b25cca45cb6420ee7e7eb02
Dre Johnson <dre@phreshr.com>
2020-12-06 20:45:53 +0800

    clean up

--- src/App.svelte
+++ src/App.svelte
@@ -21,19 +21,6 @@
 	// 	SNOWPACK_FAUNADB_SERVER_SECRET,
 	// } = import.meta.env;
 
-	const firebaseConfig = {
-		apiKey: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
-		authDomain: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
-		databaseURL: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_DATABASE_URL,
-		projectId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
-		storageBucket: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-		appId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_APP_ID,
-		measurementId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_MEASUREMENT_ID,
-	}
-
-	if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
-
 	authStore.subscribe(async user => {
 		token = user?.token as string
 	})
