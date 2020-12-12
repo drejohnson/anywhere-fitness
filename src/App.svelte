3894533e78b5b53406858ea6ad3fbd8c2267c468
Dre Johnson <dre@phreshr.com>
2020-12-12 12:24:47 +0800

    get fauna secret from custom claims

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { Router, Route } from "svelte-navigator";
-	import firebase from "firebase/app";
 	import { createClient, setClient } from "@urql/svelte";
-	import { authStore } from "@stores/auth";;
+	import { state } from './auth'
 	import TailwindCss from "@components/TailwindCss.svelte";
 	import InlineSvg from "@components/InlineSvg.svelte";
 	import Header from "@components/Header.svelte";
@@ -15,28 +14,30 @@
 	const Explore = () => import("@routes/Explore.svelte");
 	const Auth = () => import("@routes/Auth.svelte");
 
-	let token: string
+	let userSecret: string
 
-	// const {
-	// 	SNOWPACK_FAUNADB_SERVER_SECRET,
-	// } = import.meta.env;
+	$: userSecret = $state.context.user?.["https://fauna.com/user_metadata"].secret
 
-	authStore.subscribe(async user => {
-		token = user?.token as string
-	})
+	const {
+		SNOWPACK_FAUNADB_SERVER_SECRET,
+	} = import.meta.env;
 
 	const endpoint = 'https://graphql.fauna.com/graphql';
 
-	const client = createClient({
-		url: endpoint,
-		fetchOptions: () => {
-			return {
-				headers: { authorization: token ? `Bearer ${token}` : "" },
-			};
-		},
-	});
+	$: {
+		console.log('userSecret', userSecret)
+		const client = createClient({
+			url: endpoint,
+			fetchOptions: () => {
+				const token = userSecret || SNOWPACK_FAUNADB_SERVER_SECRET
+				return {
+					headers: { authorization: `Bearer ${token}` },
+				};
+			},
+		});
 
-	setClient(client);
+		setClient(client);
+	}
 </script>
 
 <style lang="postcss">
