955c3e095e82e7e4e93f860779eb5d2758ff34b4
Dre Johnson <dre@phreshr.com>
2020-12-17 05:39:32 +0800

    export geolocaction utils

---
+++ src/utils/geolocate/index.ts
@@ -0,0 +1,9 @@
+import { getAutocomplete } from "./getAutocomplete";
+import { getCoordinates } from "./getCoordinates";
+import { getCurrentLocation } from "./getCurrentLocation";
+
+export {
+  getCurrentLocation,
+  getCoordinates,
+  getAutocomplete
+}
