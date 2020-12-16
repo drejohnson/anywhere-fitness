2e3c58973c4756ebe48265336446ba4e033a5a6e
Dre Johnson <dre@phreshr.com>
2020-12-17 05:41:01 +0800

    add misc style adjustments

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-import lazyload from '@actions/lazyload'
+  import lazyload from '@actions/lazyload';
 
-
+  export let height = 'min-h-screen';
   export let image: string;
-  export let image_placeholder: string = "";
-  export let styles: string = "";
-  export let gradient = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))"
-
+  export const image_placeholder: string = '';
+  export let styles: string = '';
+  export let gradient = 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))';
 </script>
 
 <style lang="postcss">
@@ -14,8 +13,8 @@ import lazyload from '@actions/lazyload'
 
 <section
   use:lazyload
-  class="grid md:auto-cols-fr gap-x-4 min-h-screen bg-cover bg-center items-center justify-items-center relative {styles}"
+  class="grid grid-cols-1 gap-x-4 {height} bg-cover bg-center items-center justify-items-center relative {styles}"
   style="background-image: {gradient}, url({image})"
   {...$$restProps}>
-  <slot></slot>
+  <slot />
 </section>
--- src/components/LandingContent.svelte
+++ src/components/LandingContent.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+  export let styleClasses = '';
 </script>
 
 <style lang="postcss">
 </style>
 
 <section
-  class="grid {$$slots.image ? 'grid-rows-2 md:grid-rows-1 md:grid-cols-2 bg-gray-200' : 'md:grid-cols-1'} gap-x-4">
+  class="grid {$$slots.image ? 'grid-rows-2 md:grid-rows-1 md:grid-cols-2 bg-gray-200' : 'md:grid-cols-1'} gap-x-4 {styleClasses}">
   {#if $$slots.image}
     <slot name="image" />
   {/if}
 
   <div
-    class="flex flex-col justify-center items-center my-10 {!$$slots.image ? 'md:px-24' : ''} px-4">
+    class="flex flex-col justify-center items-center py-10 px-4 {!$$slots.image ? 'md:px-24' : ''}">
     <slot name="heading" />
     <slot name="text" />
     <slot name="cta" />
